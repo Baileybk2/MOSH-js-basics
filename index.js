4a7bdca967f277b6ba09a2d2313b14f82e909d50
BaileyBeck <baileybk2@gmail.com>
2024-09-19 14:54:36 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+let isApproved = false
+let firstName = undefined
+let selectedColor = null
+
+let person = {
+  name: "Bailey",
+  age: 30,
+}
+
+person.name = "Nicky"
+
+person["name"] = "Mary"
+
+console.log(person.name)
+
+let selectedColors = ["red", "blue"]
+selectedColors[2] = 1
+console.log(selectedColors)
