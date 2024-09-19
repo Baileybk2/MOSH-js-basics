e46346aeeb0eefe358299c1fd308820356e5a33e
BaileyBeck <baileybk2@gmail.com>
2024-09-19 15:37:09 -0400

    pre-quiz work

--- index.js
+++ index.js
@@ -18,3 +18,32 @@ console.log(person.name)
 let selectedColors = ["red", "blue"]
 selectedColors[2] = 1
 console.log(selectedColors)
+
+// Ternary Operator
+
+let points = 110
+let type = points > 100 ? "gold" : "silver"
+
+console.log(type)
+
+// Logical Operators
+
+console.log(true && true)
+
+let highIncome = true
+let goodCreditScore = true
+let eligibleForLoan = highIncome && goodCreditScore
+console.log("Eligible", eligibleForLoan)
+
+let applicationRefused = !eligibleForLoan
+console.log("Application refused", applicationRefused)
+
+// the result of a logical expression is not necessarily a true or false...
+
+false || true
+// true
+false || "Bailey"
+// 'Bailey'
+false || 1
+// 1
+// Falsy & Truthy --> if something isn't Falsy, it's Truthy
