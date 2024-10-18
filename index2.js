037d174a09f493b1c68447146c8755932e2bf983
BaileyBeck <baileybk2@gmail.com>
2024-10-18 10:24:43 -0400

    array intro exercises

---
+++ index2.js
@@ -0,0 +1,22 @@
+// ARRAYS
+
+// Adding Elements
+
+const numbers = [3, 4]
+
+// End
+
+numbers.push(5, 6)
+console.log(numbers)
+
+// Beginning
+
+numbers.unshift(1, 2)
+console.log(numbers)
+
+// Middle
+
+numbers.splice(2, 0, "a", "b")
+console.log(numbers)
+
+// Finding Elements
